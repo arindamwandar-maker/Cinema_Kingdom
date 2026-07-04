52c11459933327ab7b03699badc65cf60c6d44d0
arindamwandar-maker <arindam.wandar@gmail.com>
2026-07-04 23:46:34 +0530

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .start import StartHandler
-from .search import SearchHandler
-from .request import RequestHandler
-from .callback import CallbackHandler
-from .storage_handler import StorageHandler
