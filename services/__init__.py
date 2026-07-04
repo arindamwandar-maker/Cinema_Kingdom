3f276f57c5e7f634fe8e9cdc7554404fe9be1e7e
arindamwandar-maker <arindam.wandar@gmail.com>
2026-07-04 23:47:01 +0530

    Delete services directory

--- services/__init__.py
+++
@@ -1,6 +0,0 @@
-from .force_join import ForceJoinService
-from .shortener import ShortenerService
-from .storage import StorageService
-from .cooldown import CooldownService
-from .auto_delete import AutoDeleteService
-from .logger import setup_logger
